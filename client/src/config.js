0dbedb23617b4bb5f19a4b730f8d11a18e59ff60
AhmedEssamYouns <ahmedessamyounsa@gmail.com>
2024-10-06 18:16:07 +0300

    deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,6 @@
-let BASE_URL;
-
+let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
-} else {
-  BASE_URL = 'https://cafe-connect-production.up.railway.app'; 
 }
 
 export { BASE_URL };
